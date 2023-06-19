8b1d6070f8900e5218b5d595f3506795526529a3
Armela Shehaj <77633687+armelashehaj@users.noreply.github.com>
2023-06-19 11:36:02 +0200

    First test commit

---
+++ src/main/java/com/example/AlumniInternProject/entity/Role.java
@@ -0,0 +1,11 @@
+package com.example.AlumniInternProject.entity;
+
+import jakarta.persistence.Entity;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+public class Role {
+    public static String test(){
+        return "test1";
+    }
+}

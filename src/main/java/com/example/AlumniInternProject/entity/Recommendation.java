2d5c43983d7f15984935a390541125a2a815009c
dea <zeqirajdea@gmail.com>
2023-07-07 10:59:23 +0200

    added recommendation cruds

--- src/main/java/com/example/AlumniInternProject/entity/Recommendation.java
+++ src/main/java/com/example/AlumniInternProject/entity/Recommendation.java
@@ -3,7 +3,6 @@ package com.example.AlumniInternProject.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -12,7 +11,6 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-@RequiredArgsConstructor
 
 @Table(name = "reviews")
 public class Recommendation extends IdBaseEntity {
@@ -30,12 +28,10 @@ public class Recommendation extends IdBaseEntity {
     @JoinColumn(name = "recommended_user_id")
     private User recommendedUser;
 
-
-
     public Recommendation(User recommender, User recommendedUser, String comment, LocalDateTime timestamp) {
         this.recommender = recommender;
         this.recommendedUser = recommendedUser;
         this.comment = comment;
-        this.timestamp = timestamp;
+        this.timestamp = LocalDateTime.now();
     }
 }
--- src/main/java/com/example/AlumniInternProject/recommendation/RecommendationServiceImpl.java
+++ src/main/java/com/example/AlumniInternProject/recommendation/RecommendationServiceImpl.java
@@ -1,32 +1,67 @@
-//package com.example.AlumniInternProject.recommendation;
-//
-//import com.example.AlumniInternProject.entity.Recommendation;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class RecommendationServiceImpl implements RecommendationService{
-//    private final RecommendationRepository recommendationRepository;
-//
-//    public RecommendationGetDto save(RecommendationDto recommendationDto) {
-//        var recommendation = new Recommendation(
-//                recommendationDto.getRecommender(),
-//                recommendationDto.getRecommendedUser(),
-//                recommendationDto.getComment(),
-//                recommendationDto.getTimestamp()
-//        );
-//        var saved = recommendationRepository.save(recommendation);
-//        return map(saved);
-//    }
-//
-//    private RecommendationGetDto map(Recommendation recommendation){
-//        var dto = new RecommendationGetDto();
-//        dto.setId(recommendation.getId());
-//        dto.setRecommender(recommendation.getRecommender());
-//        dto.setRecommendedUser(recommendation.getRecommendedUser());
-//        dto.setComment(recommendation.getComment());
-//        dto.setTimestamp(recommendation.getTimestamp());
-//        return dto;
-//    }
-//}
+package com.example.AlumniInternProject.recommendation;
+
+import com.example.AlumniInternProject.entity.Recommendation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationServiceImpl implements RecommendationService{
+    private final RecommendationRepository recommendationRepository;
+
+    @Override
+    public RecommendationGetDto save(RecommendationDto recommendationDto) {
+        var recommendation = new Recommendation(
+                recommendationDto.getRecommender(),
+                recommendationDto.getRecommendedUser(),
+                recommendationDto.getComment(),
+                recommendationDto.getTimestamp()
+        );
+        var saved = recommendationRepository.save(recommendation);
+        return map(saved);
+    }
+
+    @Override
+    public List<RecommendationGetDto> findAll(){
+        return recommendationRepository.findAll()
+                .stream()
+                .map(recommendation -> map(recommendation))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public RecommendationGetDto findById(UUID id){
+        var optional = recommendationRepository.findById(id);
+        if (optional.isPresent()){
+            return map(optional.get());
+        }
+        throw new RuntimeException("Recommendation not found");
+    }
+
+    @Override
+    public RecommendationGetDto update(UUID id, RecommendationDto dto){
+        var skill = recommendationRepository.findById(id).orElseThrow(RuntimeException::new);
+        skill.setComment(dto.getComment());
+        var saved = recommendationRepository.save(skill);
+        return map(saved);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        recommendationRepository.deleteById(id);
+    }
+
+    private RecommendationGetDto map(Recommendation recommendation){
+        var dto = new RecommendationGetDto();
+        dto.setId(recommendation.getId());
+        dto.setRecommender(recommendation.getRecommender().);
+        dto.setRecommendedUser(recommendation.getRecommendedUser());
+        dto.setComment(recommendation.getComment());
+        dto.setTimestamp(recommendation.getTimestamp());
+        return dto;
+    }
+}

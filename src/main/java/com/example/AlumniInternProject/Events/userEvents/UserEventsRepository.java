0a1c995fd9d0bb188fad53efffab196d5634cd01
dea <zeqirajdea@gmail.com>
2023-07-10 11:14:14 +0200

    fixed user events query

--- src/main/java/com/example/AlumniInternProject/Events/userEvents/UserEventsRepository.java
+++ src/main/java/com/example/AlumniInternProject/Events/userEvents/UserEventsRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface UserEventsRepository extends JpaRepository<UserEvents, UUID> {
-    @Query("SELECT u FROM User u INNER JOIN UserEvents ue ON u.id = ue.user.id WHERE ue.event.id = :eventId")
+    @Query("SELECT u FROM User u INNER JOIN UserEvents ue ON u.id = ue.user.id WHERE ue.eventSpecifics.id = :eventId")
     List<User> findUsersByEventId(@Param("eventId") UUID eventId);
 }

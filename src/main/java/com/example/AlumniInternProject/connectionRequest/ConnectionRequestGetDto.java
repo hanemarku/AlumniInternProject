51a1ec867e160d7eff6157cc2a91a3ea8e4fd656
hane.marku <hana.marku22@gmail.com>
2023-07-12 12:23:35 +0200

    connection requests

---
+++ src/main/java/com/example/AlumniInternProject/connectionRequest/ConnectionRequestGetDto.java
@@ -0,0 +1,2 @@
+package com.example.AlumniInternProject.connectionRequest;public class ConnectionRequestGetDto {
+}
---
+++ src/main/java/com/example/AlumniInternProject/entity/ConnectionRequest.java
@@ -0,0 +1,2 @@
+package com.example.AlumniInternProject.entity;public class ConnectionRequest {
+}

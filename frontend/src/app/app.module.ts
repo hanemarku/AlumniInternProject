0b0b5c0ea2725f2a8e3c7e74622cd9487c8be62e
hane.marku <hana.marku22@gmail.com>
2023-07-19 10:36:40 +0200

    test

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListUsersComponent } from './user/list-users/list-users.component';
+import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DataTablesModule,
     ListUsersComponent
   ],
   imports: [
--- frontend/src/app/user/list-users/list-users.component.ts
+++ frontend/src/app/user/list-users/list-users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDataService } from 'src/app/services/user-service/user-data.service';
+import Swal from 'sweetalert2';
+
 
 export class UserList{
   constructor(
@@ -43,6 +45,73 @@ export class ListUsersComponent implements OnInit{
         this.users = response;
       }
     )
+    
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      processing: true,
+    };
   }
+
+  getCountryName(country: any): string {
+    return country ? country.name : 'Unknown';
+  }
+
+  getRoleName(role: any): string {
+    return role ? role.name : 'Unknown';
+  }
+
+  openUserModal(user: UserList): void {
+    this.selectedUser = user;
+  }
+
+
+  
+  toggleUserStatus(userId: string, event: any): void {
+    const enabled = event.target.checked;
+    this.userService.updateEnabledStatus(userId, enabled).subscribe(
+      response => {
+        const user = this.users.find(u => u.id === userId);
+        console.log("user-> " + user?.email);
+        if (user) {
+
+          user.enabled = response.enabled;
+          console.log("user.enabled-> " + user.enabled);
+        }
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  removeUser(userId: string): void {
+    this.userService.deleteUser(userId).subscribe(
+      () => {
+        this.users = this.users.filter(user => user.id !== userId);
+        Swal.fire('Success', 'User removed successfully!', 'success');
+      },
+      error => {
+        Swal.fire('Error', 'An error occurred while removing the user.', 'error');
+      }
+    );
+  }
+
+  openConfirmDialog(userId: string): void {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this user!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, remove it!',
+      cancelButtonText: 'Cancel',
+    }).then(result => {
+      if (result.isConfirmed) {
+        this.removeUser(userId);
+      }
+    });
+  }
+
+  
 
 }

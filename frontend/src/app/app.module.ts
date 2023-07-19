bdaf235541230791ded5607a12000888d28e9c74
hane.marku <hana.marku22@gmail.com>
2023-07-19 10:39:48 +0200

    lib

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListUsersComponent } from './user/list-users/list-users.component';
+
+  
 import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataTablesModule,
     ListUsersComponent
   ],
   imports: [
     BrowserModule,
+    DataTablesModule,
     AppRoutingModule
   ],
   providers: [],

b6eae3065be460dd089e3a62cf5cda4b9cce4a30
hane.marku <hana.marku22@gmail.com>
2023-07-19 10:05:29 +0200

    user list

--- frontend/src/app/services/user-service/user-data.service.ts
+++ frontend/src/app/services/user-service/user-data.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserList } from 'src/app/user/list-users/list-users.component';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserDataService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  listAllUsers(){
+    return this.http.get<UserList[]>("http://localhost:8080/api/v1/users");
+  }
+
+  updateEnabledStatus(id: string, status: boolean): Observable<any> {
+    return this.http.get<any>(`http://localhost:8080/api/v1/users/${id}/enabled/${status}`);
+  }
+  
+
+  deleteUser(userId: string): Observable<any> {
+    const url = `http://localhost:8080/api/v1/users/${userId}`;
+    return this.http.delete(url);
+  }
+  
+  
 }
--- frontend/src/app/user/list-users/list-users.component.ts
+++ frontend/src/app/user/list-users/list-users.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserDataService } from 'src/app/services/user-service/user-data.service';
+
+export class UserList{
+  constructor(
+   public id : string,
+   public firstname: string,
+   public lastname: string,
+   public email: string,
+   public enabled: boolean,
+   public birthday: Date,
+   public profilePicUrl: string,
+   public phoneNumber: string,
+   public city: string,
+   public country: string,
+   public bio: string,
+   public skills: { name: string }[], 
+   public interests: {  name: string }[], 
+   public role: string
+
+  ){}
+}
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
   styleUrls: ['./list-users.component.sass']
 })
-export class ListUsersComponent {
+export class ListUsersComponent implements OnInit{
+
+  users: UserList[] = [];
+  dtOptions: DataTables.Settings = {};
+  selectedUser: UserList | null = null;
+
+  constructor(
+    private userService: UserDataService
+  ){}
+
+  ngOnInit(): void {
+    this.userService.listAllUsers().subscribe(
+      response => {
+        console.log(response);
+        this.users = response;
+      }
+    )
+  }
 
 }

6e956df26c892a7caf29671e345a5e3c3c90c490
dea <zeqirajdea@gmail.com>
2023-07-27 09:40:05 +0200

    updated app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,18 +7,31 @@ import { ListUsersComponent } from './user/list-users/list-users.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { DataTablesModule } from 'angular-datatables';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SkillListComponent } from './skill/skill.component';
+import { EditSkillDialogComponent } from './skill/edit-skill-dialog/edit-skill-dialog.component';
+import { EditInterestDialogComponent } from './interest/edit-interest-dialog/edit-interest-dialog.component';
+import { InterestListComponent } from './interest/interest.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListUsersComponent
+    ListUsersComponent,
+    SkillListComponent,
+    EditSkillDialogComponent,
+    EditInterestDialogComponent,
+    InterestListComponent
   ],
   imports: [
     BrowserModule,
     DataTablesModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]

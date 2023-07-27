32f03cc9275dec38d254fe7f1be3f546e7b07850
hane.marku <hana.marku22@gmail.com>
2023-07-27 09:57:50 +0200

    module.ts'

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { DataTablesModule } from 'angular-datatables';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material/dialog';
 import { SkillListComponent } from './skill/skill.component';
 import { EditSkillDialogComponent } from './skill/edit-skill-dialog/edit-skill-dialog.component';
 import { EditInterestDialogComponent } from './interest/edit-interest-dialog/edit-interest-dialog.component';
 import { InterestListComponent } from './interest/interest.component';
-
+import { SingupComponent } from './user/singup/singup.component';
+import { CountryComponent } from './country/country.component';
+import { CityComponent } from './city/city.component';
+import { EducationComponent } from './education/education.component';
+import { CountryCitySelectorComponent } from './country-city-selector/country-city-selector.component';
+import { SkillInterestComponentComponent } from './skill-interest-component/skill-interest-component.component';
+import { SkillSearchComponent } from './skill-search/skill-search.component';
+import { FilterPipe } from './filter.pipe';
+import { InterestSearchComponent } from './interest-search/interest-search.component';
+import { EducationFormComponent } from './education-form/education-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -22,7 +33,17 @@ import { InterestListComponent } from './interest/interest.component';
     SkillListComponent,
     EditSkillDialogComponent,
     EditInterestDialogComponent,
-    InterestListComponent
+    InterestListComponent,
+    SingupComponent,
+    CountryComponent,
+    CityComponent,
+    EducationComponent,
+    CountryCitySelectorComponent,
+    SkillInterestComponentComponent,
+    SkillSearchComponent,
+    FilterPipe,
+    InterestSearchComponent,
+    EducationFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,7 +52,10 @@ import { InterestListComponent } from './interest/interest.component';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

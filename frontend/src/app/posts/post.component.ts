eb68bb2b89b0dae69c1b5221685ca1ab0372a622
henrialiaj <henrialiaj21@gmail.com>
2023-08-09 09:06:08 +0200

    h

---
+++ frontend/src/app/posts/post.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
+export class PostComponent {
+
+    constructor(
+        public
+    ){}
+}

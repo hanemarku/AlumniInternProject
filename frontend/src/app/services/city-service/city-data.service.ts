8195bd597823f6e36cee0e1658ddb689393c680f
dea <zeqirajdea@gmail.com>
2023-08-16 12:01:36 +0200

    added getCitiesByCountry

--- frontend/src/app/services/city-service/city-data.service.ts
+++ frontend/src/app/services/city-service/city-data.service.ts
@@ -27,7 +27,11 @@ export class CityDataService {
 
   updateCity(cityId: string, updatedCity: any){
     return this.http.patch(`http://localhost:8080/api/v1/cities/${cityId}`, updatedCity);
-  } 
+  }
+  
+  getCitiesByCountry(countryId: string){
+    return this.http.get<any>(`http://localhost:8080/api/v1/cities/countries/${countryId}`);
+  }
 
 
 }
